e2853ae26534a2b1d85f9911aac97114fa2a74e9
adelga <angeldelgado7@gmail.com>
2019-05-04 12:08:36 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SignUpComponent} from "./sign-up/sign-up.component";
+import {HomepageComponent} from "./homepage/homepage.component";
+import {QuizComponent} from "./quiz/quiz.component";
+import {ModeratorComponent} from "./moderator/moderator.component";
+import {ChatComponent} from "./chat/chat.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomepageComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'quiz', component: QuizComponent},
+  {path: 'moderator', component: ModeratorComponent},
+  {path: 'chat', component: ChatComponent},
+  {}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { ModeratorComponent } from './moderator/moderator.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { ChatComponent } from './chat/chat.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    QuizComponent,
+    ModeratorComponent,
+    WelcomeComponent,
+    ChatComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
