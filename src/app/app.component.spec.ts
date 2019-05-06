6304d9e7c967129d199b63557becee9fdf897264
adelga <angeldelgado7@gmail.com>
2019-05-06 08:59:03 -0400

    fixed spelling

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'clear-preassure'`, () => {
+  it(`should have as title 'clear-pressure'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('clear-preassure');
+    expect(app.title).toEqual('clear-pressure');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to clear-preassure!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to clear-pressure!');
   });
 });
